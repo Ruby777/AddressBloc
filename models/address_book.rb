e9adb3455ec35524bc63e971d8dc08398136e57e
Lucy <lucynn11@gmail.com>
2018-04-30 08:30:27 +0400

    address-bloc-searching-assignment complete

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,7 +8,7 @@ class AddressBook
         @entries = []
     end
 
-def add_entry(name, phone_number, email)
+  def add_entry(name, phone_number, email)
 
     index = 0
     entries.each do |entry|
@@ -22,15 +22,15 @@ def add_entry(name, phone_number, email)
     entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-def remove_entry(name, phone_number, email)
+  def remove_entry(name, phone_number, email)
     delete_entry = nil
 
     @entries.each do |entry|
         if name == entry.name && phone_number == entry.phone_number && email == entry.email
             delete_entry = entry
         end
-     end
-     @entries.delete(delete_entry)
+    end
+    @entries.delete(delete_entry)
   end
 
   def import_from_csv(file_name)
@@ -61,6 +61,15 @@ def remove_entry(name, phone_number, email)
         end
     end
 
+    return nil
+  end
+
+  def iterative_search(name)
+    @entries.each do |entry|
+        if entry.name == name
+            return entry  
+        end  
+    end
     return nil
   end
 
